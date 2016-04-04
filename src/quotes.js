cc8c58d97bb3000102e5c4dee3dde5e1bc66356c
Caue Alves <cauealveseng@gmail.com>
2016-04-03 22:03:02 -0300

    Include a initial file

---
+++ src/quotes.js
@@ -0,0 +1,48 @@
+/*!
+ * @author Cauê Alves https://github.com/cauealves
+ */
+
+(function(window){
+    var Quotes = {
+        options: {
+            sleep: 2, // seconds
+            repeat: false
+        }
+    };
+
+    var prefix = '[Quotes.js]';
+
+    Quotes.error = function error(message) {
+        throw new Error(prefix + ': ' + message);
+    };
+
+    Quotes.warning = function warning(message) {
+        if (console && console.warn) 
+            console.warn(prefix + ': ' + message);
+    };
+
+    Quotes.start = function(quotes, options) {
+        if (typeof quotes !== 'object' || !quotes.length)
+            this.error('You must enter a valid object of citations!');
+
+        Quotes.options = merge(options, Quotes.options);
+
+        console.log(Quotes.options);
+    };
+
+    function merge(options, defaults) {
+        Object.keys(options).forEach(function(key){
+            var value = options[key];
+            if (defaults[key])
+                defaults[key] = value;
+        });
+
+        return defaults;
+    };
+
+    if (typeof window === 'object') {
+        window.Quotes = Quotes;
+    };
+})(function(){
+    return this;
+}());

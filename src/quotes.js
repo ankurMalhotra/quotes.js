d07ad8126c60face64746ba8f94218a5bf3bafb8
Caue Alves <cauealveseng@gmail.com>
2016-04-03 22:20:53 -0300

    Include the selector option and validate it

--- src/quotes.js
+++ src/quotes.js
@@ -28,6 +28,11 @@
         if (typeof phrases !== 'object' || !phrases.length)
             this.error('You should pass a list of valid phrases containing the quotes you want to display!');
 
+        var invalidSelector = (typeof options.selector !== 'string' || !options.selector.length);
+        
+        if (invalidSelector || null === document.querySelector(options.selector)) 
+            this.error('Please, use a valid selector.');
+        
         Quotes.phrases = phrases;
         Quotes.options = merge(options, Quotes.options);
     };
